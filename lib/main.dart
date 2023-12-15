3907a3b9613af89d6f2b31b5c6cac20574c8867b
NancyMohamed1910 <143906014+NancyMohamed1910@users.noreply.github.com>
2023-12-15 18:12:37 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 //import 'package:e_commerce/view/screens/signUp.dart';
+<<<<<<< HEAD
 import 'package:e_commerce/providers/category.provider.dart';
 import 'package:e_commerce/seeder/data.seeder.dart';
 import 'package:e_commerce/view/pages/splash_page.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+=======
+
+import 'package:e_commerce/providers/advertise.providers.dart';
+import 'package:e_commerce/providers/category.provider.dart';
+import 'package:e_commerce/providers/product.providers.dart';
+>>>>>>> 8d4e3443a035508035756a9863385f806628df6d
 import 'package:e_commerce/services/preference.services.dart';
 import 'package:e_commerce/utils/theme.utils.dart';
 import 'package:e_commerce/view/pages/main_page.dart';
 import 'package:e_commerce/view/pages/master_page.dart';
-
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+<<<<<<< HEAD
   // await PreferenceSrevice.init();
   await DataSeeder.loadData;
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,6 +47,21 @@ void main() async {
       )
     ], child: MyApp()),
   );
+=======
+  await PreferenceSrevice.init();
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => ProductProvider()),
+    ChangeNotifierProvider(create: (_) => CategoryProvider()),
+    /*FutureProvider<List<String>?>(
+      create: (_) => CategoryProvider().getTitle(),
+      initialData: null,
+      catchError: (_, err) {
+        return [];
+      },
+    ),*/
+    ChangeNotifierProvider(create: (_) => AdvertiseProvider()),
+  ], child: const MyApp()));
+>>>>>>> 8d4e3443a035508035756a9863385f806628df6d
 }
 
 class MyApp extends StatefulWidget {
@@ -49,7 +72,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
   @override
   void initState() {
     //PreferenceServices.init();
