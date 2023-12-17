fe40ee2a346e503df504b6cb9eabd1b085c2f126
NancyMohamed1910 <143906014+NancyMohamed1910@users.noreply.github.com>
2023-12-17 05:57:11 +0200

    add video folder

--- lib/view/pages/main_page.dart
+++ lib/view/pages/main_page.dart
@@ -33,68 +33,75 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 100,
-            // width: 367,
-            color: Color(0xffF5F6F8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ListView(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    physics: ClampingScrollPhysics(),
-                    children: AuthPage.values
-                        .map((e) => Padding(
-                              padding: EdgeInsets.all(8),
-                              child: InkWell(
-                                focusColor: Color(0xffF5F6F8),
-                                highlightColor: Color(0xffF5F6F8),
-                                hoverColor: Color(0xffF5F6F8),
-                                splashColor: Color(0xffF5F6F8),
-                                onTap: () {
-                                  selectedPage = e;
-                                  if (selectedPage == AuthPage.signUp) {
-                                    pageController.jumpToPage(0);
-                                  } else if (selectedPage == AuthPage.logIn) {
-                                    pageController.jumpToPage(1);
-                                  } else
-                                    pageController.jumpToPage(2);
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              //width: 367,
+              color: Color(0xffF5F6F8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        children: AuthPage.values
+                            .map((e) => Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: InkWell(
+                                    focusColor: Color(0xffF5F6F8),
+                                    highlightColor: Color(0xffF5F6F8),
+                                    hoverColor: Color(0xffF5F6F8),
+                                    splashColor: Color(0xffF5F6F8),
+                                    onTap: () {
+                                      selectedPage = e;
+                                      if (selectedPage == AuthPage.signUp) {
+                                        pageController.jumpToPage(0);
+                                      } else if (selectedPage ==
+                                          AuthPage.logIn) {
+                                        pageController.jumpToPage(1);
+                                      } else
+                                        pageController.jumpToPage(2);
 
-                                  setState(() {});
-                                },
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      e.name,
-                                      style: selectedPage == e
-                                          ? TextStyle(
-                                              fontSize: 30,
-                                              color: Colors.black,
-                                              fontWeight: FontWeight.w700)
-                                          : TextStyle(
-                                              fontSize: 30,
-                                              color: Colors.grey,
-                                              fontWeight: FontWeight.w500),
+                                      setState(() {});
+                                    },
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          e.name,
+                                          style: selectedPage == e
+                                              ? TextStyle(
+                                                  fontSize: 30,
+                                                  color: Colors.black,
+                                                  fontWeight: FontWeight.w700)
+                                              : TextStyle(
+                                                  fontSize: 30,
+                                                  color: Colors.grey,
+                                                  fontWeight: FontWeight.w500),
+                                        ),
+                                        SizedBox(width: 30),
+                                      ],
                                     ),
-                                    SizedBox(width: 30),
-                                  ],
-                                ),
-                              ),
-                            ))
-                        .toList()),
-              ],
+                                  ),
+                                ))
+                            .toList()),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-              child: PageView(
-                  controller: pageController,
-                  children: WidgetData.myLoginWidget))
-        ],
+            Expanded(
+                child: PageView(
+                    controller: pageController,
+                    children: WidgetData.myLoginWidget))
+          ],
+        ),
       ),
     );
   }
