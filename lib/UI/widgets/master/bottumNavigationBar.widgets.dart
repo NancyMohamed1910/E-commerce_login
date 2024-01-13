c2f495e4304fd0cd8b5ab50aa03679429c3a3d4d
NancyMohamed1910 <143906014+NancyMohamed1910@users.noreply.github.com>
2024-01-13 03:56:01 +0200

    commit

--- lib/UI/widgets/master/bottumNavigationBar.widgets.dart
+++ lib/UI/widgets/master/bottumNavigationBar.widgets.dart
@@ -1,7 +1,9 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:e_commerce/providers/cart.providers.dart';
 import 'package:e_commerce/utils/colors.util.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
+import 'package:provider/provider.dart';
 
 class BottomNavigationBarWidget extends StatefulWidget {
   final int selectedIndex;
@@ -23,7 +25,7 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
   @override
   Widget build(BuildContext context) {
     return AnimatedBottomNavigationBar.builder(
-      height: 49,
+      height: 55,
       onTap: (index) {
         widget.onTap.call(index);
       },
@@ -39,39 +41,102 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
         return Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Icon(
-              index == 0
-                  ? LineIcons.home
-                  : index == 1
-                      ? LineIcons.search
-                      : index == 2
-                          ? LineIcons.shoppingCart
-                          : index == 3
-                              ? LineIcons.user
-                              : LineIcons.bars,
-              size: 25,
-              color:
-                  isActive ? ColorsUtil.activeIconColor : ColorsUtil.iconColor,
-            ),
-            Text(
-              index == 0
-                  ? 'Home'
-                  : index == 1
-                      ? 'Search'
-                      : index == 2
-                          ? 'cart'
-                          : index == 3
-                              ? 'Profile'
-                              : 'More',
-              style: TextStyle(
-                color: isActive
-                    ? ColorsUtil.activeIconColor
-                    : ColorsUtil.iconColor,
+            Expanded(
+                flex: 2,
+                child: IconButton(
+                    onPressed: () {
+                      widget.onTap.call(index);
+                    },
+                    icon: getIcon(index, isActive))),
+            Expanded(
+              flex: 1,
+              child: Text(
+                index == 0
+                    ? 'Home'
+                    : index == 1
+                        ? 'Search'
+                        : index == 2
+                            ? 'cart'
+                            : index == 3
+                                ? 'Profile'
+                                : 'More',
+                style: TextStyle(
+                  color: isActive
+                      ? ColorsUtil.activeIconColor
+                      : ColorsUtil.iconColor,
+                ),
               ),
-            )
+            ),
           ],
         );
       }),
     );
+  }
+
+  Widget getIcon(int index, bool isActive) {
+    if (index == 0) {
+      return Icon(
+        LineIcons.home,
+        size: 25,
+        color: isActive ? ColorsUtil.activeIconColor : ColorsUtil.iconColor,
+      );
+    } else if (index == 1) {
+      return Icon(
+        LineIcons.search,
+        size: 25,
+        color: isActive ? ColorsUtil.activeIconColor : ColorsUtil.iconColor,
+      );
+    } else if (index == 2) {
+      return Row(
+        children: [
+          //////
+          StreamBuilder(
+              stream: Provider.of<CartProvider>(context).cartStream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  int quantity = 0;
+                  if (snapshot.data?.data()?['items'] != null) {
+                    for (Map<String, dynamic> item
+                        in snapshot.data?.data()?['items']) {
+                      quantity += (item['quantity'] as int);
+                    }
+
+                    return Badge(
+                      smallSize: 15,
+                      backgroundColor: ColorsUtil.badgeColor,
+                      label: Text('$quantity'),
+                    );
+                  } else {
+                    return const SizedBox(
+                      width: 10,
+                    );
+                  }
+                } else {
+                  return const SizedBox(
+                    width: 10,
+                  );
+                }
+              }),
+
+          Icon(
+            LineIcons.shoppingCart,
+            size: 25,
+            color: isActive ? ColorsUtil.activeIconColor : ColorsUtil.iconColor,
+          )
+        ],
+      );
+    } else if (index == 3) {
+      return Icon(
+        LineIcons.user,
+        size: 25,
+        color: isActive ? ColorsUtil.activeIconColor : ColorsUtil.iconColor,
+      );
+    } else {
+      return Icon(
+        LineIcons.bars,
+        size: 25,
+        color: isActive ? ColorsUtil.activeIconColor : ColorsUtil.iconColor,
+      );
+    }
   }
 }
